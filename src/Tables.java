707d8351710338a9345b28f17c0b4a4f690a1a35
MIBN21 <92128168+MIBN21@users.noreply.github.com>
2021-10-17 13:49:39 +0800

    Add files via upload

---
+++ src/Tables.java
@@ -0,0 +1,21 @@
+package Project;
+
+public class Tables {
+	private int tableid;
+	private int tablesize;
+	private boolean isReserved;
+	
+	public int getTableId()
+	{
+		return this.tableid;
+	}
+	public int getTableSize()
+	{	
+		return this.tablesize;
+	}
+	public boolean getStatus()
+	{
+		return this.isReserved;
+	}
+
+}
