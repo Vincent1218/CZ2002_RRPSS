fa262ca85a5ee458cd3431ab61f1bbabc805e4e3
Vincent1218 <vicentchai@hotmail.com>
2021-11-14 00:45:19 +0800

    Changes

--- src/ReservationList.java
+++ src/ReservationList.java
@@ -6,7 +6,6 @@ import java.text.SimpleDateFormat;
 
 public class ReservationList {
 	private ArrayList<Reservation> resList;
-	private int rescount = 0;
 	private String[] timeArray = {"10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30","20:00","20:30","21:00"};
 
 	public ReservationList() {
@@ -83,8 +82,6 @@ public class ReservationList {
 				System.out.println("Incorrect Entry. Return to main program..");
 				return;
 			}
-			//int resId = rescount + 1;
-			//rescount++;
 			int resId = resList.size() + 1;
 			Reservation newRes = new Reservation(name, contact, pax, timeArray[timeChoice-1], tableId, resId);
 			resList.add(newRes);
